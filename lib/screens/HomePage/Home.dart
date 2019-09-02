8f156e5afc7c992fe9e010d5a2a9be01304c3b70
edwardcruzcruz <edward.cruzcruz@hotmail.com>
2019-09-02 15:23:21 -0500

    Banner agregado y singleschildscrollview parcialmente justado

--- lib/screens/HomePage/Home.dart
+++ lib/screens/HomePage/Home.dart
@@ -18,7 +18,8 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Inicio"),
+        title: new Image.asset('assets/logo_clinica.png', fit: BoxFit.cover,),
+        centerTitle: true,
         backgroundColor: Color.fromRGBO(19, 206, 177, 100),
         actions: <Widget>[
           IconButton(
@@ -31,209 +32,224 @@ class Home extends StatelessWidget {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            //crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  new Card(
-                    elevation: 0,
-                    color: Colors.transparent,
-                    child: new Row(
-                      children: <Widget>[
-                        new Column(
-                          children: <Widget>[
-                            new Container(
-                              margin: const EdgeInsets.only(top: 30.0,bottom: 10.0),
-                              width: 130.0,
-                              height: 130.0,
-                              decoration: new BoxDecoration(
-                                image: DecorationImage(
-                                  image: new AssetImage(
-                                      'assets/agendamiento.png'),
-                                  fit: BoxFit.none,
-                                ),
-                                shape: BoxShape.circle,
-                                color: Color.fromRGBO(204,192, 2,100),
-                              ),
-                            ),
-                            new Container(
-                              child: new Text('Agendar Cita'),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  new Card(
-                    elevation: 0,
-                    color: Colors.transparent,
-                    child: new Row(
-                      children: <Widget>[
-                        new Column(
-                          children: <Widget>[
-                            new Container(
-                              margin: const EdgeInsets.only(top: 30.0,bottom: 10.0),
-                              width: 130.0,
-                              height: 130.0,
-                              decoration: new BoxDecoration(
-                                image: DecorationImage(
-                                  image: new AssetImage(
-                                      'assets/calendario.png'),
-                                  fit: BoxFit.none,
-                                ),
-                                shape: BoxShape.circle,
-                                color: Color.fromRGBO(75, 54, 154, 100),
-                              ),
-                            ),
-                            new Container(
-                              child: new Text('Mis Citas'),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  //Text('25 min'),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  new Card(
-                    elevation: 0,
-                    color: Colors.transparent,
-                    child: new Row(
-                      children: <Widget>[
-                        new Column(
-                          children: <Widget>[
-                            new Container(
-                              margin: const EdgeInsets.only(top: 30.0,bottom: 10.0),
-                              width: 130.0,
-                              height: 130.0,
-                              decoration: new BoxDecoration(
-                                image: DecorationImage(
-                                  image: new AssetImage(
-                                      'assets/historial.png'),
-                                  fit: BoxFit.none,
-                                ),
-                                shape: BoxShape.circle,
-                                color: Colors.cyan,
-                              ),
-                            ),
-                            new Container(
-                              child: new Text('Historial Clinico'),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  new Card(
-                    elevation: 0,
-                    color: Colors.transparent,
-                    child: new Row(
-                      children: <Widget>[
-                        new Column(
-                          children: <Widget>[
-                            new Container(
-                              margin: const EdgeInsets.only(top: 30.0,bottom: 10.0),
-                              width: 130.0,
-                              height: 130.0,
-                              decoration: new BoxDecoration(
-                                image: DecorationImage(
-                                  image: new AssetImage(
-                                      'assets/recetas.png'),
-                                  fit: BoxFit.none,
-                                ),
-                                shape: BoxShape.circle,
-                                color: Colors.red,
-                              ),
-                            ),
-                            new Container(
-                              child: new Text('Recetas'),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  new Card(
-                    elevation: 0,
-                    color: Colors.transparent,
-                    child: new Row(
-                      children: <Widget>[
-                        new Column(
-                          children: <Widget>[
-                            new Container(
-                              margin: const EdgeInsets.only(top: 30.0,bottom: 10.0),
-                              width: 130.0,
-                              height: 130.0,
-                              decoration: new BoxDecoration(
-                                image: DecorationImage(
-                                  image: new AssetImage(
-                                      'assets/resultados.png'),
-                                  fit: BoxFit.none,
-                                ),
-                                shape: BoxShape.circle,
-                                color: Color.fromRGBO(22, 48, 207, 100),
-                              ),
-                            ),
-                            new Container(
-                              child: new Text('Mis Resultados'),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  new Card(
-                    elevation: 0,
-                    color: Colors.transparent,
-                    child: new Row(
-                      children: <Widget>[
-                        new Column(
-                          children: <Widget>[
-                            new Container(
-                              margin: const EdgeInsets.only(top: 30.0,bottom: 10.0),
-                              width: 130.0,
-                              height: 130.0,
-                              decoration: new BoxDecoration(
-                                image: DecorationImage(
-                                  image: new AssetImage(
-                                      'assets/carrito.png'),
-                                  fit: BoxFit.none,
-                                ),
-                                shape: BoxShape.circle,
-                                color: Color.fromRGBO(240, 126, 26,100),
-                              ),
-                            ),
-                            new Container(
-                              child: new Text('Pagos'),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  //Icon(Icons.restaurant, color: Colors.green[500]),
-                  //Text('FEEDS:'),
-                  //Text('4-6'),
-                ],
-              ),
-            ],
+      body: Column(
+        children: <Widget>[
+          new Banner(
+          message: "",//mensaje esquina superior derecha
+            location: BannerLocation.topEnd,
+            color: Colors.red,
+            child: Container(
+              margin: const EdgeInsets.only(left:0.0,top:10.0,right: 0.0,bottom: 0.0),
+              color: Colors.blue,
+              height: 100,
+              child: Center(child: Text("Hello, banner!"),),
+            ),
           ),
-        ),
+          new SingleChildScrollView(
+            child: Container(
+              //padding: EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                //crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      new Card(
+                        elevation: 0,
+                        color: Colors.transparent,
+                        child: new Row(
+                          children: <Widget>[
+                            new Column(
+                              children: <Widget>[
+                                new Container(
+                                  margin: const EdgeInsets.only(top: 30.0,bottom: 10.0),
+                                  width: 130.0,
+                                  height: 130.0,
+                                  decoration: new BoxDecoration(
+                                    image: DecorationImage(
+                                      image: new AssetImage(
+                                          'assets/agendamiento.png'),
+                                      fit: BoxFit.none,
+                                    ),
+                                    shape: BoxShape.circle,
+                                    color: Color.fromRGBO(204,192, 2,100),
+                                  ),
+                                ),
+                                new Container(
+                                  child: new Text('Agendar Cita'),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      new Card(
+                        elevation: 0,
+                        color: Colors.transparent,
+                        child: new Row(
+                          children: <Widget>[
+                            new Column(
+                              children: <Widget>[
+                                new Container(
+                                  margin: const EdgeInsets.only(top: 30.0,bottom: 10.0),
+                                  width: 130.0,
+                                  height: 130.0,
+                                  decoration: new BoxDecoration(
+                                    image: DecorationImage(
+                                      image: new AssetImage(
+                                          'assets/calendario.png'),
+                                      fit: BoxFit.none,
+                                    ),
+                                    shape: BoxShape.circle,
+                                    color: Color.fromRGBO(75, 54, 154, 100),
+                                  ),
+                                ),
+                                new Container(
+                                  child: new Text('Mis Citas'),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      //Text('25 min'),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      new Card(
+                        elevation: 0,
+                        color: Colors.transparent,
+                        child: new Row(
+                          children: <Widget>[
+                            new Column(
+                              children: <Widget>[
+                                new Container(
+                                  margin: const EdgeInsets.only(top: 30.0,bottom: 10.0),
+                                  width: 130.0,
+                                  height: 130.0,
+                                  decoration: new BoxDecoration(
+                                    image: DecorationImage(
+                                      image: new AssetImage(
+                                          'assets/historial.png'),
+                                      fit: BoxFit.none,
+                                    ),
+                                    shape: BoxShape.circle,
+                                    color: Colors.cyan,
+                                  ),
+                                ),
+                                new Container(
+                                  child: new Text('Historial Clinico'),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      new Card(
+                        elevation: 0,
+                        color: Colors.transparent,
+                        child: new Row(
+                          children: <Widget>[
+                            new Column(
+                              children: <Widget>[
+                                new Container(
+                                  margin: const EdgeInsets.only(top: 30.0,bottom: 10.0),
+                                  width: 130.0,
+                                  height: 130.0,
+                                  decoration: new BoxDecoration(
+                                    image: DecorationImage(
+                                      image: new AssetImage(
+                                          'assets/recetas.png'),
+                                      fit: BoxFit.none,
+                                    ),
+                                    shape: BoxShape.circle,
+                                    color: Colors.red,
+                                  ),
+                                ),
+                                new Container(
+                                  child: new Text('Recetas'),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      new Card(
+                        elevation: 0,
+                        color: Colors.transparent,
+                        child: new Row(
+                          children: <Widget>[
+                            new Column(
+                              children: <Widget>[
+                                new Container(
+                                  margin: const EdgeInsets.only(top: 30.0,bottom: 10.0),
+                                  width: 130.0,
+                                  height: 130.0,
+                                  decoration: new BoxDecoration(
+                                    image: DecorationImage(
+                                      image: new AssetImage(
+                                          'assets/resultados.png'),
+                                      fit: BoxFit.none,
+                                    ),
+                                    shape: BoxShape.circle,
+                                    color: Color.fromRGBO(22, 48, 207, 100),
+                                  ),
+                                ),
+                                new Container(
+                                  child: new Text('Mis Resultados'),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      new Card(
+                        elevation: 0,
+                        color: Colors.transparent,
+                        child: new Row(
+                          children: <Widget>[
+                            new Column(
+                              children: <Widget>[
+                                new Container(
+                                  margin: const EdgeInsets.only(top: 30.0,bottom: 10.0),
+                                  width: 130.0,
+                                  height: 130.0,
+                                  decoration: new BoxDecoration(
+                                    image: DecorationImage(
+                                      image: new AssetImage(
+                                          'assets/carrito.png'),
+                                      fit: BoxFit.none,
+                                    ),
+                                    shape: BoxShape.circle,
+                                    color: Color.fromRGBO(240, 126, 26,100),
+                                  ),
+                                ),
+                                new Container(
+                                  child: new Text('Pagos'),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      //Icon(Icons.restaurant, color: Colors.green[500]),
+                      //Text('FEEDS:'),
+                      //Text('4-6'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
